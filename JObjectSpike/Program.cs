4a3a6aa2564e8809b703a83281c0a04bfa5a78c1
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-05-04 09:01:21 -0700

    Initial commit

---
+++ JObjectSpike/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+
+namespace JObjectSpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var json = System.IO.File.ReadAllText("sample.json");
+            var target = JObject.Parse(json);
+            foreach(var element in target)
+            {
+                Console.WriteLine($"{element.Key}: endpoint: {element.Value.Value<JToken>("endpoint")}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
